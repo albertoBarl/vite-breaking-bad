65324b2160b3033acc1020b750a5072c0e255cf5
albertoBarl <alberto.barletta00@gmail.com>
2023-01-17 10:29:24 +0100

    fixed order

--- src/assets/datas/store.js
+++ src/assets/datas/store.js
@@ -2,6 +2,6 @@ import { reactive } from "vue";
 export const store = reactive({
   url: "https://db.ygoprodeck.com/api/v7/cardinfo.php?archetype=",
   cardList: [],
+  selectedGen: "Dark Magician",
   isLoaded: false,
-  selectedGen: "Alien",
 });
